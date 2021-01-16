ce0d511c3f5804c041dc9e24d17988c7a76a5bbf
Donut5896 <Nutchanokchon5@gmail.com>
2021-01-16 17:31:13 +0700

    16/01/21

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 
+
+
+
+
+const hide = document.getElementById('hide');
+const show = document.getElementById('show');
+
+
+
+
+
 function myFunction(){
 
     
     let x = document.getElementById("myInput");
     if(x.type === "password"){
         x.type = "text";
-        document.getElementById("hide").style.display = "inline-block";
-        document.getElementById("show").style.display = "none";
+        hide.style.display =   "none"
+        show.style.display = "inline-block"
+       
     }else{
-         document.getElementById("hide").style.display = "none";
-        document.getElementById("show").style.display = "inline-block";
+        x.type = "password";
+        show.style.display =   "none"
+        hide.style.display = "inline-block"
+        
+       
     }
+   
 }
+
